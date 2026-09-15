d05aec16a982d1f03682616dc9ce7fc655b58f7e
Vladimir Kukushkin <kukushechkin@apple.com>
2026-09-15 11:49:03 +0100

    use propoer logger for telemetry service group

--- SwiftServerTodos.docc/codelistings/4-telemetry.1.swift
+++ SwiftServerTodos.docc/codelistings/4-telemetry.1.swift
@@ -73,7 +73,8 @@ func configureTelemetry(_ config: ConfigReader) async throws -> (Logger, some Se
             otelMetricsBackend.service,
             otelTracingBackend.service,
             systemMetricsMonitor,
-        ]
+        ],
+        logger: logger
     )
 
     return (logger, telemetryService)
